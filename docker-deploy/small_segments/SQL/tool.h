ce85c3d7343981a918b181c432e53ff316a25bf4
dukeVCM <tl330@duke.edu>
2023-04-01 17:19:51 -0400

    move

--- docker-deploy/small_segments/SQL/tool.h
+++
@@ -1,5 +0,0 @@
-#include "head.h"
-
-string float_to_string(float value);
-int getTime();
-string read_file_to_string(const std::string& filename) ;
--- docker-deploy/small_segments/client/connect_func.h
+++
@@ -1,5 +0,0 @@
-#include "head.h"
-
-int setup_server(const char * port);
-int setup_client(const char * hostname, const char * port);
-int accept_server(int socket_fd);
