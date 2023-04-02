a69bded256729c80681c2449f15563f99aec2488
dukeVCM <tl330@duke.edu>
2023-04-02 17:33:20 -0400

    fix to L61

--- small_segments/sql/query_funcs.cpp
+++ small_segments/sql/query_funcs.cpp
@@ -9,7 +9,6 @@ void runSQL(string sql, connection *C){
 result selectSQL(connection *C, string sql){
   nontransaction N(*C);
   result R( N.exec( sql ));
-  N.commit();
   return R;
 }
 
@@ -45,18 +44,18 @@ void createTable(string fileName, connection *C){
 ///////////////////////////////////////////////////////////////////////////////////////
 
 string add_account(connection *C, int account_id, float balance){
-  string sql1 = "SELECT ACCOUNT_ID FROM ACCOUNT WHERE ACCOUNT_ID= "+ to_string(account_id);
-  result R=selectSQL(C,sql1);
-  if(R.size()!=0){
-    return "  <error id=\""+to_string(account_id)+"\">Account already exists</error>\n";
-  }
   if(balance<=0){
     return "  <error id=\""+to_string(account_id)+"\">Balance cannot be smaller or equal to 0</error>\n";
   }
-
-  string sql2 = "INSERT INTO ACCOUNT (ACCOUNT_ID, BALANCE) VALUES (" 
+  string sql1 = "SELECT ACCOUNT_ID FROM ACCOUNT WHERE ACCOUNT_ID= "+ to_string(account_id);
+  result R=selectSQL(C, sql1);
+  if(R.size()!=0){
+    return "  <error id=\""+to_string(account_id)+"\">Account already exists</error>\n";
+  }
+  string sql2 = "INSERT INTO ACCOUNT (ACCOUNT_ID, BALANCE) SELECT " 
                 + to_string(account_id) + "," 
-                + to_string(balance) + ");";
+                + to_string(balance) + ""
+                +"WHERE NOT EXISTS (SELECT 1 FROM ACCOUNT WHERE ACCOUNT_ID=" + to_string(account_id) + ");";
   runSQL(sql2,C);
   return "  <created id=\""+to_string(account_id)+"\"/>\n";
 }
@@ -68,14 +67,16 @@ string add_stock(connection *C, int account_id, string symbol, int amount){
     return "    <error sym=\""+to_string(symbol)+"\" id=\""+to_string(account_id)+"\">Amount cannot be smaller or equal to 0</error>\n";
   }
   //check account exist
-  string sql1 = "SELECT ACCOUNT_ID FROM ACCOUNT WHERE ACCOUNT_ID= "+ quoteStr(C, to_string(account_id));
+  string sql1 = "SELECT ACCOUNT_ID FROM ACCOUNT WHERE ACCOUNT_ID= "+ quoteStr(C, to_string(account_id))
+                + " FOR UPDATE";
   result R=selectSQL(C, sql1 );
   if(R.size()==0){
     return "    <error sym=\""+to_string(symbol)+"\" id=\""+to_string(account_id)+"\">Account does not exist</error>\n";
   }
   
   string sql2 = "SELECT STOCK_ID FROM STOCK \
-                WHERE ACCOUNT_ID= " + quoteStr(C, to_string(account_id)) + " AND SYMBOL = " + quoteStr(C, symbol);
+                WHERE ACCOUNT_ID= " + quoteStr(C, to_string(account_id)) + " AND SYMBOL = " + quoteStr(C, symbol)
+                + " FOR UPDATE";
   result R2 = selectSQL(C, sql2);
   if(R2.size()==0){//stock does not exist->insert
     string sql = "INSERT INTO STOCK (ACCOUNT_ID, SYMBOL, AMOUNT) VALUES (" 
@@ -179,7 +180,8 @@ string cancel(connection *C, int account_id, int order_id){
   //already canceled
   string sql1 = "SELECT * \
                 FROM ORDERS \
-                WHERE ORDER_ID = " + to_string(order_id) + " AND STATES = " + quoteStr(C, "open");
+                WHERE ORDER_ID = " + to_string(order_id) + " AND STATES = " + quoteStr(C, "open") 
+                + " FOR UPDATE";
   result R=selectSQL(C, sql1);
   if(R.size()<=0){
     return "  <canceled id=\""+to_string(order_id)+"\">\n" + query_body(C,order_id) + "  </canceled>\n";
@@ -195,7 +197,8 @@ string cancel(connection *C, int account_id, int order_id){
   //get refund value
   string sql3 = "SELECT * \
                 FROM ORDERS \
-                WHERE ORDER_ID = " + to_string(order_id) + " AND STATES = " + quoteStr(C, "cancel");
+                WHERE ORDER_ID = " + to_string(order_id) + " AND STATES = " + quoteStr(C, "cancel")
+                + " FOR UPDATE";
   result R2=selectSQL(C, sql3);
   //refund
   if(R[0][5].as<string>()=="buy"){
@@ -278,16 +281,18 @@ int add_buy_order(connection *C, int account_id, string symbol, int amount, floa
   string sql1 = "SELECT * \
                 FROM ORDERS \
                 WHERE TYPES= 'sell' AND SYMBOL= " + quoteStr(C, symbol) +" AND PRICE <= " + to_string(price) + " AND STATES = 'open'\
-                ORDER BY PRICE ASC, TIMESEC ASC";
+                ORDER BY PRICE ASC, TIMESEC ASC"
+                + " FOR UPDATE";
   result R_sell=selectSQL(C, sql1);
   
-  result num=selectSQL(C, "SELECT lastval();");
+  result num=selectSQL(C, "SELECT lastval() FOR UPDATE;");
   result::const_iterator c = R_sell.begin();
   while(true){
     //get buy order
     string sql2 = "SELECT * \
                   FROM ORDERS \
-                  WHERE UNIQUE_ID = " + num[0][0].as<string>();
+                  WHERE UNIQUE_ID = " + num[0][0].as<string>()
+                  + " FOR UPDATE";
     result R_buy=selectSQL(C, sql2);
 
     if(amount == 0){
@@ -320,16 +325,18 @@ int add_sell_order(connection *C, int account_id, string symbol, int amount, flo
   string sql1 = "SELECT * \
                 FROM ORDERS \
                 WHERE TYPES= 'buy' AND SYMBOL= " + quoteStr(C, symbol) +" AND PRICE >= " + to_string(price) + " AND STATES = 'open'\
-                ORDER BY PRICE DESC, TIMESEC ASC";
+                ORDER BY PRICE DESC, TIMESEC ASC"
+                + " FOR UPDATE";
   result R_buy=selectSQL(C, sql1);
   
-  result num=selectSQL(C, "SELECT lastval();");
+  result num=selectSQL(C, "SELECT lastval() FOR UPDATE;");
   result::const_iterator c = R_buy.begin();
   while(true){
     //get sell order
     string sql2 = "SELECT * \
                   FROM ORDERS \
-                  WHERE UNIQUE_ID = " + num[0][0].as<string>();
+                  WHERE UNIQUE_ID = " + num[0][0].as<string>()
+                  + " FOR UPDATE";
     result R_sell=selectSQL(C, sql2);
 
     if(amount == 0){ //done
@@ -355,13 +362,14 @@ int add_sell_order(connection *C, int account_id, string symbol, int amount, flo
 }
 
 string add_order(connection *C, int account_id, string symbol, int amount, float price){
-  string sql1 = "SELECT ACCOUNT_ID, BALANCE FROM ACCOUNT WHERE ACCOUNT_ID= "+ to_string(account_id);
+  if(amount<=0){
+    return "  <error sym=\""+to_string(symbol)+"\" amount=\""+to_string(amount)+"\" limit=\""+to_string(static_cast<int>(price))+"\">Amount cannot be smaller or equal to 0</error>\n";
+  }
+
+  string sql1 = "SELECT ACCOUNT_ID, BALANCE FROM ACCOUNT WHERE ACCOUNT_ID= "+ to_string(account_id) + " FOR UPDATE";
   result R=selectSQL(C, sql1);
   if(R.size()==0){
     return "  <error sym=\""+to_string(symbol)+"\" amount=\""+to_string(amount)+"\" limit=\""+to_string(static_cast<int>(price))+"\">Account does not exist</error>\n";
-  }
-  if(amount<=0){
-    return "  <error sym=\""+to_string(symbol)+"\" amount=\""+to_string(amount)+"\" limit=\""+to_string(static_cast<int>(price))+"\">Amount cannot be smaller or equal to 0</error>\n";
   }
   string type= "buy";
   if(price<0){
@@ -384,7 +392,8 @@ string add_order(connection *C, int account_id, string symbol, int amount, float
   else{//sell
     string sql3 = "SELECT STOCK_ID, AMOUNT \
                     FROM STOCK \
-                    WHERE ACCOUNT_ID= "+ to_string(account_id) + " AND SYMBOL = " + quoteStr(C,symbol);
+                    WHERE ACCOUNT_ID= "+ to_string(account_id) + " AND SYMBOL = " + quoteStr(C,symbol)
+                    + " FOR UPDATE";
     result R2=selectSQL(C,sql3);
     if(R2.size()<1 || amount > R2.begin()[1].as<int>()){
       return "  <error sym=\""+to_string(symbol)+"\" amount=\""+to_string(amount)+"\" limit=\""+to_string(static_cast<int>(price))+"\">Not enough stocks</error>\n";
@@ -399,192 +408,4 @@ string add_order(connection *C, int account_id, string symbol, int amount, float
     }
   }  
 }
-/*
-void insertAccount(string fileName, connection *C){
-  string line;
-  int account_id;
-  float balance;
-  ifstream f (fileName.c_str());
-  if (f.is_open()){
-      while(getline(f,line)){
-        stringstream ss;
-        ss << line;
-        ss >> account_id >> balance;
-        add_account(C, account_id , balance);
-      }
-      f.close();
-  }
-  else{
-    cout << "Cannot open " + fileName <<endl;
-    exit(1);
-  }
-}
 
-void insertStock(string fileName, connection *C){
-  string line, symbol;
-  int account_id, amount;
-  ifstream f (fileName.c_str());
-  if (f.is_open()){
-      while(getline(f,line)){
-        stringstream ss;
-        ss << line;
-        ss >> account_id >> symbol >> amount;
-        add_stock(C, account_id, symbol, amount);
-      }
-      f.close();
-  }
-  else{
-    cout << "Cannot open " + fileName <<endl;
-    exit(1);
-  }
-}
-
-void insertOrder(string fileName, connection *C){
-  string line, symbol, states;
-  int account_id, amount;
-  float price;
-  ifstream f (fileName.c_str());
-  if (f.is_open()){
-      while(getline(f,line)){
-        stringstream ss;
-        ss << line;
-        ss >> account_id >> symbol >> amount >> price >> states;
-        add_order(C, account_id, symbol, amount, price, states);
-      }
-      f.close();
-  }
-  else{
-    cout << "Cannot open " + fileName <<endl;
-    exit(1);
-  }
-}
-*/
-
-/*
-void addSizeCondition(string * sql, int use, bool* notFiltered, string name, int min, int max){
-  if(use){
-    if(*notFiltered){
-      sql->append("WHERE ");
-      *notFiltered=false;
-    }
-    else{
-      sql->append("AND ");
-    }
-    sql->append(name+" >= "+to_string(min)+" AND "+name+" <= "+to_string(max)+" ");
-  }
-}
-void addSizeCondition(string * sql, int use, bool* notFiltered, string name, double min, double max){
-  if(use){
-    if(*notFiltered){
-      sql->append("WHERE ");
-      *notFiltered=false;
-    }
-    else{
-      sql->append("AND ");
-    }
-    sql->append(name+" >= "+to_string(min)+" AND "+name+" <= "+to_string(max)+" ");
-  }
-}
-
-void query1(connection *C,
-  int use_mpg, int min_mpg, int max_mpg,
-  int use_ppg, int min_ppg, int max_ppg,
-  int use_rpg, int min_rpg, int max_rpg,
-  int use_apg, int min_apg, int max_apg,
-  int use_spg, double min_spg, double max_spg,
-  int use_bpg, double min_bpg, double max_bpg
-  )
-{
-  string sql = "SELECT * FROM PLAYER ";  
-  bool notFiltered=true;
-  addSizeCondition(&sql, use_mpg, &notFiltered, "MPG", min_mpg, max_mpg);
-  addSizeCondition(&sql, use_ppg, &notFiltered, "PPG", min_ppg, max_ppg);
-  addSizeCondition(&sql, use_rpg, &notFiltered, "RPG", min_rpg, max_rpg);
-  addSizeCondition(&sql, use_apg, &notFiltered, "APG", min_apg, max_apg);
-  addSizeCondition(&sql, use_spg, &notFiltered, "SPG", min_spg, max_spg);
-  addSizeCondition(&sql, use_bpg, &notFiltered, "BPG", min_bpg, max_bpg);
-
-  nontransaction N(*C);
-  result R( N.exec( sql ));
-  cout <<"PLAYER_ID TEAM_ID UNIFORM_NUM FIRST_NAME LAST_NAME MPG PPG RPG APG SPG BPG" << endl;
-  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<int>() << " ";
-    cout << c[1].as<int>() << " ";
-    cout << c[2].as<int>() << " ";
-    cout << c[3].as<string>() << " ";
-    cout << c[4].as<string>() << " ";
-    cout << c[5].as<int>() << " ";
-    cout << c[6].as<int>() << " ";
-    cout << c[7].as<int>() << " ";
-    cout << c[8].as<int>() << " ";
-    cout << std::fixed << std::setprecision(1) << c[9].as<double>() << " ";
-    cout << std::fixed << std::setprecision(1) << c[10].as<double>() << endl;
-  }
-}
-
-
-void query2(connection *C, string team_color)
-{
-  work W(*C);
-  string sql = "SELECT TEAM.NAME FROM TEAM, COLOR WHERE TEAM.COLOR_ID=COLOR.COLOR_ID AND COLOR.NAME = "+ W.quote(team_color);
-  W.commit();
-
-  nontransaction N(*C);
-  result R( N.exec( sql ));
-  cout <<"NAME" << endl;
-  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<string>() << endl;
-  }
-}
-
-
-void query3(connection *C, string team_name)
-{
-  work W(*C);
-  string sql = "SELECT PLAYER.FIRST_NAME, PLAYER.LAST_NAME FROM PLAYER, TEAM WHERE TEAM.TEAM_ID=PLAYER.TEAM_ID AND TEAM.NAME = "+ W.quote(team_name)+" ORDER BY PLAYER.PPG DESC";
-  W.commit();
-
-  nontransaction N(*C);
-  result R( N.exec( sql ));
-  cout <<"FIRST_NAME LAST_NAME" << endl;
-  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<string>() << ' ';
-    cout << c[1].as<string>() << endl;
-  }
-}
-
-
-void query4(connection *C, string team_state, string team_color)
-{
-  work W(*C);
-  string sql = "SELECT PLAYER.UNIFORM_NUM, PLAYER.FIRST_NAME, PLAYER.LAST_NAME FROM PLAYER, TEAM, STATE, COLOR WHERE TEAM.TEAM_ID=PLAYER.TEAM_ID AND TEAM.STATE_ID=STATE.STATE_ID AND TEAM.COLOR_ID=COLOR.COLOR_ID AND STATE.NAME = "+ W.quote(team_state)+" AND COLOR.NAME = "+ W.quote(team_color);
-  W.commit();
-
-  nontransaction N(*C);
-  result R( N.exec( sql ));
-  cout <<"UNIFORM_NUM FIRST_NAME LAST_NAME" << endl;
-  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<int>() << ' ';
-    cout << c[1].as<string>() << ' ';
-    cout << c[2].as<string>() << endl;
-  }
-}
-
-
-void query5(connection *C, int num_wins)
-{
-  work W(*C);
-  string sql = "SELECT PLAYER.FIRST_NAME, PLAYER.LAST_NAME, TEAM.NAME, TEAM.WINS FROM PLAYER, TEAM WHERE TEAM.TEAM_ID=PLAYER.TEAM_ID AND TEAM.WINS > "+ to_string(num_wins);
-  W.commit();
-
-  nontransaction N(*C);
-  result R( N.exec( sql ));
-  cout <<"FIRST_NAME LAST_NAME NAME WINS" << endl;
-  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<string>() << ' ';
-    cout << c[1].as<string>() << ' ';
-    cout << c[2].as<string>() << ' ';
-    cout << c[3].as<int>() << endl;
-  }
-}
-*/
