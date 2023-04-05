90e230999311df984a92ff63ce17168f368dc910
dukeVCM <tl330@duke.edu>
2023-04-04 20:35:31 -0400

    connection done

--- docker-deploy/main.cpp
+++ docker-deploy/main.cpp
@@ -32,6 +32,22 @@ int main(int argc, char *argv[])
   createTable("file/order.sql", C);
   C->disconnect();
 
+  
+  /*for(int i=0;i<3;i++){
+    connection *C4;
+    try{ 
+      C4 = new connection("dbname=EXCHANGE_SERVER user=postgres password=passw0rd");
+      if (C4->is_open()) {
+      } else {
+        cout << "Can't open database" << endl;
+        return 1;
+      } 
+    } catch (const std::exception &e){
+      cerr << e.what() << std::endl;
+      return 1;
+    }
+  }*/
+
   const char * port = PORT;
   Server * myServer = new Server(port);
   myServer->run();
--- testing/scability/run.sh
+++ testing/scability/run.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 fileNum=10
-maxThread=50
+maxThread=100
 start=$(date +%s.%N)
 
 
